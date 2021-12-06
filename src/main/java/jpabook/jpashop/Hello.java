37e7fa5778ad5b82dfcda00846f99d843ca445ab
LeeSangBum <dltkdqja101@gmail.com>
2021-12-06 23:19:52 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hello {
+    private String data;
+}
